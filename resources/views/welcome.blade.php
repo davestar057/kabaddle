30e4392743d96a04936415070c95dbb5cbe0f537
David Haw <david@kabaddle.com>
2021-05-03 15:18:56 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>KABADDLE - Web Design, SEO and Marketing Leeds</title>
+    <meta name="description"
+          content="Leeds based digital agency - Premium web design & seo services shouldn't cost a fortune! Making processes simple, clear and affordable"/>
+    <meta property="og:locale" content="en_GB"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="KABADDLE - Web Design, SEO and Marketing Leeds"/>
+    <meta property="og:description"
+          content="Leeds based digital agency - Premium web design & seo services shouldn't cost a fortune! Making processes simple, clear and affordable"/>
+    <meta property="og:url" content="https://www.kabaddle.com/"/>
+    <meta property="og:site_name" content="Kabaddle"/>
+    <meta property="og:site_name" content="Kabaddle"/>
+    <meta property="article:publisher" content="https://www.facebook.com/kabaddle"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta name="robots" content="index, follow"/>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;700&display=swap" rel="stylesheet">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-JRYCXJT5GE"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'G-JRYCXJT5GE');
+    </script>
+
+</head>
+<body>
+<video autoplay loop playsinline muted>
+    <source src="/videos/kabaddle_1.mp4" type="video/mp4">
+</video>
+<div class="viewport-header">
+    <div class="logo">
+        <img src="/img/brand/logo-sm.png" alt="Kabaddle" class="img-fluid px-4">
+    </div>
+
+    <p class="weDo">apps . digital . mobile . web . seo</p>
+    <p class="agency">WE BELIEVE CUTTING EDGE WEB DEVELOPMENT & SEO SHOULDN'T HAVE TO COST A FORTUNE OR BE
+        COMPLICATED</p>
+
+    <div style="position: absolute; bottom:2%">
+        <p class="address" style=" font-size: 13px;"><strong style="color:#f72b60">t</strong> 0113 873 0397 |
+            <strong style="color:#f72b60">e</strong> hello@kabaddle.com</p>
+        <p class="address" style="margin-bottom:0;font-size: 12px; ">Suite 21, Madison Offices, Radley House, Richardshaw Rd, Pudsey LS28 6LE
+        <p>
+        <p style="font-size:11px; margin-top:10px; color: #aca7a6">&copy; KABADDLE LTD - registered company 12956148</p>
+    </div>
+</div>
+
+</body>
+</html>
